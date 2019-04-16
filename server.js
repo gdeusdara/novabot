0d21913196cf4142a7b7decbdb9e87a6d0220ffc
Guilherme Deusdará <guibanci@gmail.com>
2019-04-16 14:44:47 -0300

    mudando para .env

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const env = require('dotenv');
 const app = express();
 
+env.config();
+
 const port = process.env.PORT || 5000;
-const db = 'mongodb://heroku_598rhfhh:1ukc1g7soq5so1dk6uvr6i3tr8@ds151853.mlab.com:51853/heroku_598rhfhh';
 const Schema = mongoose.Schema;
 
-mongoose.connect(db, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({ extended: true }));
     
 var quote = new Schema({
@@ -17,42 +19,45 @@ var quote = new Schema({
 
 var Quotes = mongoose.model('novatics-quotes', quote);
 
+
+
 //Route
 app.post('/', (req, res) => {
-  console.log(req.body);
-  if (req.body.text === '') {
-
-      res.send('Você não mandou nenhum quote! ):');
-  
-} else if (req.body.text.includes(' by: ')) {
-
-    var newQuote = req.body.text.split(" by: ");
+    console.log(req.body);
+    if (req.body.text === '') {
+        
+        res.send('Você não mandou nenhum quote! ):');
+        
+    } else if (req.body.text.includes(' by: ')) {
+        
+        var newQuote = req.body.text.split(" by: ");
+        
+        console.log(newQuote);
+        
+        var item = {  
+            quote: newQuote[0],  
+            auth: newQuote[1],  
+        };  
+        var data = new Quotes(item);  
+        data.save();
+        
+    } else {
+        var item = {  
+            quote: req.body.text,  
+            auth: 'sem autor',  
+        };
+        var data = new Quotes(item);  
+        data.save();
+        
+    }
     
-    console.log(newQuote);
-
-    var item = {  
-        quote: newQuote[0],  
-        auth: newQuote[1],  
-    };  
-    var data = new Quotes(item);  
-    data.save();
-
-} else {
-    var item = {  
-        quote: req.body.text,  
-        auth: 'sem autor',  
-    };
-    var data = new Quotes(item);  
-    data.save();
-
-}
-
-res.send('Your new Quote: ' + item.quote + '\nautor: ' + item.auth);
+    res.send('Your new Quote: ' + item.quote + '\nautor: ' + item.auth);
     
 })
 //Start server
 app.listen(port, (req, res) => {
     console.log(`server listening on port: ${port}`)
+    console.log(process.env.SLACK_TOKEN);
 });
 
 app.get('/', (req, res) => {
@@ -63,7 +68,7 @@ app.get('/', (req, res) => {
     
     
 })
-        // Quotes.findById("5cb5ee91e7179a264cf184ca", function (err, myQuote) {
-        //     console.log(myQuote);
+// Quotes.findById("5cb5ee91e7179a264cf184ca", function (err, myQuote) {
+    //     console.log(myQuote);
         //     res.send('Quote: ' + myQuote.quote + '\nautor: ' + myQuote.auth);
         // });
