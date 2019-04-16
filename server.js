3baa2fa124cbd0e9fb91c1ba58da9cc39e7da700
Guilherme Deusdará <guibanci@gmail.com>
2019-04-16 13:56:44 -0300

    criando nova quote ao digitar comando no slack

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
@@ -21,16 +20,38 @@ var Quotes = mongoose.model('novatics-quotes', quote);
 //Route
 app.post('/', (req, res) => {
   console.log(req.body);
-  Quotes.findById("5cb5ee91e7179a264cf184ca", function (err, myQuote) {
-    console.log(myQuote);
-    res.send('Quote: ' + myQuote.quote + '\nautor: ' + myQuote.auth);
-});
+  if (req.body.text === '') {
+
+      res.send('Você não mandou nenhum quote! ):');
   
+} else if (req.body.text.includes(' by: ')) {
+
+    var newQuote = req.body.text.split(" by: ");
+    
+    console.log(newQuote);
+
+    var item = {  
+        quote: newQuote[0],  
+        auth: newQuote[1],  
+    };  
+       
+} else {
+    var item = {  
+        quote: req.body.text,  
+        auth: 'sem autor',  
+    };
+}
+
+    var data = new Quotes(item);  
+    data.save();
+    
+    res.send('Your new Quote: ' + item.quote + '\nautor: ' + item.auth);
+    
 })
 //Start server
 app.listen(port, (req, res) => {
-console.log(`server listening on port: ${port}`)
- });
+    console.log(`server listening on port: ${port}`)
+});
 
 app.get('/', (req, res) => {
     Quotes.findById("5cb5ee91e7179a264cf184ca", function (err, adventure) {
@@ -40,3 +61,7 @@ app.get('/', (req, res) => {
     
     
 })
+        // Quotes.findById("5cb5ee91e7179a264cf184ca", function (err, myQuote) {
+        //     console.log(myQuote);
+        //     res.send('Quote: ' + myQuote.quote + '\nautor: ' + myQuote.auth);
+        // });
