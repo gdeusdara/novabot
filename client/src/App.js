a891af29cef232d3ef7a521c153908f07e150e56
Guilherme Deusdará <guibanci@gmail.com>
2019-04-18 08:54:29 -0300

    posicionando imagens de perfil

--- client/src/App.js
+++ client/src/App.js
@@ -28,13 +28,16 @@ class App extends Component {
 
   render() {
     const { quotes, isLoading } = this.state;
-    console.log('opa' + quotes);
+    console.log(quotes);
     var lastItem = quotes.pop()
     var listItems = quotes.reverse().map((item) =>
       <div className="card" key={item._id}>
         <div className="card_quote"> {item.quote} </div>
         <div className="card_auth">
-          <p>{item.auth}</p>
+          <p className="profile">
+            <img src="https://secure.gravatar.com/avatar/72cb0061f2b38a8a8ad09c710d5e2877.jpg?s=72&d=https%3A%2F%2Fa.slack-edge.com%2F00b63%2Fimg%2Favatars%2Fava_0008-72.png"/>
+            {item.auth}
+          </p>
         </div>
       </div>
     );
@@ -47,7 +50,10 @@ class App extends Component {
         <header className="App-header">
           <p className="Header-help Header-help-top ">Last Quote</p>
           <p className="Header-quote">{lastItem.quote}</p>
-          <p className="Header-help">{lastItem.auth}</p>
+          <p className="Header-help">
+            <img src="https://secure.gravatar.com/avatar/72cb0061f2b38a8a8ad09c710d5e2877.jpg?s=72&d=https%3A%2F%2Fa.slack-edge.com%2F00b63%2Fimg%2Favatars%2Fava_0008-72.png"></img>
+            {lastItem.auth}
+          </p>
         </header>
         <div className="quotes-body">
           <div className="cards-list">
